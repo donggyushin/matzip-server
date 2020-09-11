64d0519853c14a849289c65c64c60d84d6fcb224
shin donggyu <donggyu9410@gmail.com>
2020-09-11 23:24:32 +0900

    Change naver open api get request

--- src/controllers/address.ts
+++ src/controllers/address.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 import axios from "axios";
 import dotenv from "dotenv";
+import { env } from "process";
 
 dotenv.config();
 
@@ -11,14 +12,19 @@ export const getAddressFromGeoLocation = async (
 ) => {
   const { longitude, latitude } = req.query;
   const naverApiQueryKey = process.env.NAVER_API_KEY_QUERIES || "";
-  const requestEndpoint = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${longitude},${latitude}&orders=legalcode,admcode,addr,roadaddr&output=json&${naverApiQueryKey}`;
+  const requestEndpoint = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${longitude},${latitude}&orders=legalcode,admcode,addr,roadaddr&output=json`;
   if (!longitude || !latitude) {
     return res.status(400).json({
       error: "클라이언트로부터 변수를 제대로 전달받지 못하였습니다.",
     });
   }
   try {
-    const response = await axios.get(requestEndpoint);
+    const response = await axios.get(requestEndpoint, {
+      headers: {
+        "X-NCP-APIGW-API-KEY-ID": process.env.NAVER_API_CLIENT_KEY,
+        "X-NCP-APIGW-API-KEY": process.env.NAVER_API_SECRET_KEY,
+      },
+    });
     const data = response.data;
     const { area1, area2, area3 } = data.results[0].region;
     const area1Name = area1.name;
